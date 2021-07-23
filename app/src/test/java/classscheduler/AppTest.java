45cd32ec1f76417be9fb4a8e9f4d25c786f6ad33
Henry Scheible <henry.scheible@gmail.com>
2021-07-23 10:25:54 -0500

    Add initial tests and workflows

--- app/src/test/java/classscheduler/AppTest.java
+++ app/src/test/java/classscheduler/AppTest.java
@@ -4,11 +4,58 @@
 package classscheduler;
 
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.NoSuchElementException;
+
+import static org.mockito.Mockito.*;
+
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    InputStream sysInBackup;
+    PrintStream sysOutBackup;
+
+    @Before
+    public void backupSystem() {
+        sysInBackup = System.in;
+        sysOutBackup = System.out;
+    }
+
+    public void replaceSystemIn(String simInput) {
+        ByteArrayInputStream in = new ByteArrayInputStream(simInput.getBytes());
+        System.setIn(in);
+    }
+
+    public ByteArrayOutputStream replaceSystemOut() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        return outContent;
+    }
+
+    @Test 
+    public void testAddsOneBlock() {
+        replaceSystemIn(CommonFixtures.classDatabase+"add\r\n0\r\nviewSchedule\r\n");
+        ByteArrayOutputStream outContent = replaceSystemOut();
+        try {
+            App.main(null);
+        } catch (NoSuchElementException e) {
+            
+        }
+        System.err.println(outContent.toString());
+        assertTrue(outContent.toString().contains("Block 1: AP Lang"));
+        assertTrue(outContent.toString().contains("Block 2: empty"));
+        assertTrue(outContent.toString().contains("Block 3: empty"));
+        assertTrue(outContent.toString().contains("Block 4: empty"));
+    }
+
+    @After
+    public void restoreSystem() {
+        System.setIn(sysInBackup);
+        System.setOut(sysOutBackup);
     }
 }
